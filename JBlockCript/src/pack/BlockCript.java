ac06d2c6b21da373824b885d162017a64cba4d2d
F3sterski <szkola121@wp.pl>
2015-03-26 11:04:05 +0100

    Dziala szyfr ecb korzystajacy z vigenere

--- JBlockCript/src/pack/BlockCript.java
+++ JBlockCript/src/pack/BlockCript.java
@@ -1,7 +1,10 @@
 package pack;
 	import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -30,8 +33,6 @@ public class BlockCript {
 	    szerokosc = Math.floor(image.getWidth()/CryptX);
 	    wysokosc = Math.floor(image.getHeight()/CryptY);
 	    
-	    System.out.println((int)szerokosc+" "+(int)wysokosc);
-	    
 	    int[][] array2D = new int[(int)(szerokosc*wysokosc)][CryptX*CryptY];
 	    int pozycja=0;
 	    int pozx=0,pozy=0;
@@ -40,7 +41,6 @@ public class BlockCript {
 		    for (int y = 0; y < CryptY; y++){
 	            for (int x = 0; x < CryptX; x++){
 	                int color = image.getRGB(CryptX*pozx+x, CryptY*pozy+y);
-	            	System.out.println((CryptX*pozx+x)+ " "+ (CryptY*pozy+y) + " "+ color);
 	                if (color==Color.BLACK.getRGB()) {
 	                    array2D[i][pozycja%(CryptX*CryptY)] = 1;
 	                } 
@@ -70,7 +70,6 @@ public class BlockCript {
 		    for (int y = 0; y < CryptY; y++){
 	            for (int x = 0; x < CryptX; x++){
 	                int color = imageArray[i][pozycja%(CryptX*CryptY)];
-	            	System.out.print(color);
 	                if (color==0) {
 	                    image.setRGB(CryptX*pozx+x,CryptY*pozy+y,Color.WHITE.getRGB());
 	                } 
@@ -80,7 +79,6 @@ public class BlockCript {
 	                pozycja++;
 	            }
 	        }
-		    System.out.println();
 	    	if(pozx<szerokosc) pozx++;
 	    	if(pozx==szerokosc){
 	    		pozy++;
@@ -91,20 +89,72 @@ public class BlockCript {
 	    ImageIO.write(image, "bmp", new File("saved.bmp"));
 	}
 	
+	public static char[] key;
+	
+	private static void ReadKey(){
+        try {
+    		
+            FileReader fileReader = new FileReader("key.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            key = line.toCharArray();
+            
+            if(key.length>25){
+	        	System.err.println("Niepoprawny klucz");
+	        	System.exit(0);
+            }
+            
+            for(int i=0;i<key.length;i++){
+    			if ((key[i] < 'a' || key[i] > 'z')){
+    				throw new IllegalArgumentException("Niepoprawny znak w kluczu");
+    			}
+            }
+            
+            bufferedReader.close();
+            
+    	}
+	    catch(FileNotFoundException ex) {
+	        System.err.println("Unable to open file 'key.txt'");   
+	        ex.printStackTrace();
+	        System.exit(0);
+	    }
+	    catch(IOException ex) {
+	        System.err.println("Error reading file 'key.txt'");  
+	        ex.printStackTrace();
+	        System.exit(0);
+	    }
+	    catch(NullPointerException ex){
+	    	ex.printStackTrace();
+	    	System.exit(0);
+	    }
+	}
+	
+	private static int[] CryptLine(int[] BeforeLine){
+		int i=0;
+		int[] AfterLine= new int[BeforeLine.length];
+		for(int j=0;j<BeforeLine.length;j++){
+			AfterLine[j]=((((BeforeLine[j])+((int)key[i]))%2));
+			i++;
+			i%=key.length;
+		}
+		return AfterLine;
+	}
+	
+	
 	
 	public static void main(String[] args) throws IOException {
+		System.out.println("Start");
+		ReadKey();
 		
-		BlockCript BC = new BlockCript(3,4);
-		
+		BlockCript BC = new BlockCript(1,20);
 		int[][] image = BC.BMPImagetoArray();
 		
-			for(int i=0;i<4;i++){
-				for(int j=0;j<12;j++){
-					System.out.print(image[i][j]);
-				}
-				System.out.println();
-			}
+		for(int j=0;j<image.length;j++){
+			image[j]=CryptLine(image[j]);
+		}
+		
 		BC.BMPArraytoImage(image);
+		System.out.println("Ready");
 	}
 
 }
